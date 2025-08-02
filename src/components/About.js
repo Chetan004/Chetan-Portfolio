0a6f2c6a7086aacdc9644818dee1f96c639577e8
Chetan004 <112242587+Chetan004@users.noreply.github.com>
2025-08-02 17:38:57 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,7 @@ function About() {
               src={profileImg}
               alt="Profile"
               className="rounded-circle shadow"
-              style={{ width: '200px', height: '200px', objectFit: 'cover' }}
+              style={{ width: '300px', height: '300px', objectFit: 'cover' }}
             />
           </div>
           <div className="col-md-8">
@@ -41,6 +41,7 @@ function About() {
 }
 
 export default About;
+
 
 
 
